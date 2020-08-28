b74ca40ec562e47a95dfd791d4990fdd97988104
PiusAremieye <ebi44ril@gmail.com>
2020-08-28 10:07:25 +0100

    solved algorithm

--- algorithm/src/main/java/com/threeline/algorithm/Encoder.java
+++ algorithm/src/main/java/com/threeline/algorithm/Encoder.java
@@ -1,5 +1,8 @@
 package com.threeline.algorithm;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,11 +16,27 @@ public class Encoder {
         return x;
     }
 
-    public static void encodeArray(int A[], int B[], int n) {
+    public static int[] encodeArray(int A[], int B[], int n) {
         for (int i = 0; i < n; i++) {
             A[i] = encodeInteger(A[i], B[i]);
-            logger.info("i {}", A[i]);
         }
+        return A;
+    }
+
+    public static String decodeArrayB(int encodedArray[], int A[]) {
+        int len = encodedArray.length;
+        int B[] = new int[len];
+        int key = 1 << (1 << (1 << 1));
+        for (int i = 0; i < len; i++) {
+            int c = (encodedArray[i] ^ A[i]) >> key;
+            B[i] = c;
+        }
+        return Arrays.toString(B);
+    }
+
+    public static void main(String[] args) {
+        int[] E = Encoder.encodeArray(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, 3);
+        System.out.println(Encoder.decodeArrayB(E, new int[] { 1, 2, 3 }));
     }
 
 }
